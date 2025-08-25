4716a4cc90797d479f55806fc3b53caab0901f73
Shivam-Dalal <shivamdalal690@gmail.com>
2025-08-26 01:56:20 +0530

    Update Q2.cpp

--- Assignment2/Q2.cpp
+++ Assignment2/Q2.cpp
@@ -1,40 +1,29 @@
 #include<iostream>
 using namespace std;
-void linearsearch(int a[],int n, int target,bool &found)
-{
-    for(int i=0;i<n;i++){
-        if(a[i]==target){
-            int pos=i;
-            cout<<"element "<<target<<"found at position "<<pos+1<<"\n";
-            found=true;
+void bubblesort(int a[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]<a[j+1]){
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
         }
     }
-}
-int countoccurences(int n,int a[],int target){
-    int count=0;
-    for (int i=0;i<n;i++){
-        if(a[i]==target) count++;
-    }
-    return count;
+
 }
 int main(){
-    bool found=false;
     int n;
     cout<<"enter number of elements in array:";
     cin>>n;
-    cout<<"enter array elements:";
     int a[n];
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    int target;
-    cout<<"enter element to search:";
-    cin>>target;
-    linearsearch(a,n,target,found);
-    if(!found) cout<<"element not found in array";
-    else {
-        // cout<<"element "<<target<<" found at pos:"<<pos+1;
-        cout<<"element "<<target<<" was repeated "<<countoccurences(n,a,target)<<" times";
+    bubblesort(a,n);
+    cout<<"sorted array:\n";
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
     }
     return 0;
 }
