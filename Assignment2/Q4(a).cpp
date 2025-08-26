8932a4f147003ac86ebc4ad22783b2c529f0cb00
Shivam-Dalal <shivamdalal690@gmail.com>
2025-08-27 00:43:57 +0530

    Create Q4(a).cpp

---
+++ Assignment2/Q4(a).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string str1,str2;
+  cout<<"enter string 1: ";
+  cin>>str1;
+  cout<<"enter string 2: ";
+  cin>>str2;
+  cout<<"the concatenation of two strings : "<<str1+str2;
+  return 0;
+}
