d821f8ce6e41635132302a811cb367b096245061
Shivam-Dalal <shivamdalal690@gmail.com>
2025-11-07 15:01:49 +0530

    Add  traversal implementation in q1.cpp
    Implemented a class for tree traversal with pre-order, in-order, and post-order methods using a stack.

--- assignment8/lab/q1.cpp
+++ assignment8/lab/q1.cpp
@@ -1,1 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+class solution{
+  vector<int> pre, in, post;
+  void allTraversals(Node* root) {
+    if (root == NULL) return;
+    stack<pair<Node*, int>> st;
+    st.push({root, 1});
+
+    while (!st.empty()) {
+        auto it = st.top();
+        st.pop();
+
+        if (it.second == 1) {
+            pre.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+            if (it.first->left != NULL)
+                st.push({it.first->left, 1});
+        }
+        else if (it.second == 2) {
+            in.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+            if (it.first->right != NULL)
+                st.push({it.first->right, 1});
+        }
+        else post.push_back(it.first->data);
+    }
+  }
+};
+int main(){
+  Node* root=new Node(1);
+  root->left=new Node(2);
+  root->left->right=new Node(4);
+  root->right=new Node(3);
+  root->right->left=new Node(5);
+  return 0;
+}
